1277fe26b6d94ecb2a492cea08154f4bca6c0f37
Alefe Serafim <correia.alefe@gmail.com>
2025-07-22 11:42:18 -0300

    Preparando para o Gcloud Run

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"os"
 
 	"github.com/aserafim/desafio_rate_limiter/internal/config"
 	"github.com/aserafim/desafio_rate_limiter/internal/limiter"
@@ -23,7 +23,10 @@ func main() {
 	handler := limiter.RateLimiterMiddleware(cfg, redisStrategy)(mux)
 
 	fmt.Println("Servidor iniciado na porta 8080...")
-	if err := http.ListenAndServe(":8080", handler); err != nil {
-		log.Fatal(err)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
 	}
+	fmt.Println("🚀 Servidor iniciado na porta " + port + "...")
+	http.ListenAndServe(":"+port, handler)
 }
